07bc20c81329d8f0e9f2899437406525f022d5e2
Rients <rientsvanburen@gmail.com>
2019-10-09 16:15:00 +0200

    karma conf aangepast

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,20 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    // updated
+    browsers: ['ChromeHeadless'],
+    // new
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
